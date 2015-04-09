d0bc36222a599fb1aec3f203d81f2bd840a69eae
Justin Nesselrotte <jnesselr@harding.edu>
2015-04-08 23:28:31 -0500

    Updating options user control

--- Ascendancy/User Controls/OptionsUserControl.xaml.cs
+++ Ascendancy/User Controls/OptionsUserControl.xaml.cs
@@ -28,7 +28,6 @@ namespace Ascendancy.User_Controls
 
             updateMusicSlider(VolumeManager.MusicVolume);
             updateSoundSlider(VolumeManager.SoundVolume);
-            UserControlAnimation.StartButtonGradientSpin(Buttons);
         }
 
         private void OptionsUserControlView_Loaded(object sender, RoutedEventArgs e)
@@ -49,16 +48,6 @@ namespace Ascendancy.User_Controls
             MusicPercentageLabel.Content = volume*100 + "%";
         }
 
-        private void OkayButton_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            Canvas sss = (Canvas)sender;
-            Storyboard localStoryboard = App.Current.FindResource("ButtonUpStoryboard") as Storyboard;
-            Storyboard.SetTarget(localStoryboard, sss.Children[1]);
-            localStoryboard.Begin();
-
-            ContentControlActions.FadeOut();
-        }
-
         private void SoundSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             updateSoundSlider(SoundSlider.Value);
@@ -72,29 +61,9 @@ namespace Ascendancy.User_Controls
             VolumeManager.MusicVolume = MusicSlider.Value;
         }
 
-
-        private void OkayButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void Okay_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            Canvas sss = (Canvas)sender;
-            Storyboard localStoryboard = App.Current.FindResource("ButtonDownStoryboard") as Storyboard;
-            Storyboard.SetTarget(localStoryboard, sss.Children[1]);
-            localStoryboard.Begin();
-
-        }
-
-        private void OkayButton_MouseEnter(object sender, MouseEventArgs e)
-        {
-            Canvas animateThisCanvas = (Canvas)sender;
-            UserControlAnimation.FadeInUserControlButton(animateThisCanvas.Children[0], true);
-            //added sound effect for the button
-            VolumeManager.play(@"Resources/Audio/UserControlButtonHover.wav");
-        }
-
-        private void OkayButton_MouseLeave(object sender, MouseEventArgs e)
-        {
-            //todo get mouse down working with this
-            Canvas animateThisCanvas = (Canvas)sender;
-            UserControlAnimation.FadeInUserControlButton(animateThisCanvas.Children[0], false);
+            ContentControlActions.FadeOut();
         }
     }
 }
