7f624f331e34886366556db88488cfe6576659f6
Justin Nesselrotte <jnesselr@harding.edu>
2015-04-11 02:05:11 -0500

    Changed Online Name Prompt buttons

--- Ascendancy/User Controls/Multiplayer/OnlineNamePromptUserControl.xaml.cs
+++ Ascendancy/User Controls/Multiplayer/OnlineNamePromptUserControl.xaml.cs
@@ -26,66 +26,13 @@ namespace Ascendancy.User_Controls.Multiplayer
         public OnlineNamePromptUserControl()
         {
             InitializeComponent();
-            UserControlAnimation.StartButtonGradientSpin(Buttons);
-        }
-
-        private void EventFilter(object sender)
-        {
-            if (sender == Back)
-            {
-                ContentControlActions.FadeOut();
-            }
-            else if (sender == EnterLobby || sender == NamePromptText)
-            {
-                if (NamePromptText.Text != "")
-                {
-                    Networkmanager.ClientName = NamePromptText.Text;
-
-                    ContentControlActions.setPopup(new OnlineLobbyUserControl());
-                }
-            }
-        }
-        
-        private void OnlineNamePromptButton_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            Canvas sss = (Canvas)sender;
-            Storyboard localStoryboard = App.Current.FindResource("ButtonUpStoryboard") as Storyboard;
-            Storyboard.SetTarget(localStoryboard, sss.Children[1]);
-            localStoryboard.Begin();
-
-            EventFilter(sender);
-        }
-
-        private void OnlineNamePromptButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            Canvas sss = (Canvas)sender;
-            Storyboard localStoryboard = App.Current.FindResource("ButtonDownStoryboard") as Storyboard;
-            Storyboard.SetTarget(localStoryboard, sss.Children[1]);
-            localStoryboard.Begin();
-
-        }
-
-        private void OnlineNamePromptButton_MouseEnter(object sender, MouseEventArgs e)
-        {
-            Canvas animateThisCanvas = (Canvas)sender;
-            UserControlAnimation.FadeInUserControlButton(animateThisCanvas.Children[0], true);
-
-            //added sound effect for the button
-            VolumeManager.play(@"Resources/Audio/UserControlButtonHover.wav");
-        }
-
-        private void OnlineNamePromptButton_MouseLeave(object sender, MouseEventArgs e)
-        {
-            //todo get mouse down working with this
-            Canvas animateThisCanvas = (Canvas)sender;
-            UserControlAnimation.FadeInUserControlButton(animateThisCanvas.Children[0], false);
         }
 
         private void NamePromptText_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
-                EventFilter(sender);
+                CheckNamePrompt();
             }
         }
 
@@ -101,6 +48,27 @@ namespace Ascendancy.User_Controls.Multiplayer
             {
                 NamePromptText.Text = "";
             }
+        }
+
+        private void Back_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            ContentControlActions.FadeOut();
+        }
+
+        private void EnterLobby_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            CheckNamePrompt();
+        }
+
+        private void CheckNamePrompt()
+        {
+            // todo strip spaces for check
+            // todo Disable button if text is empty
+            if (NamePromptText.Text == "") return;
+
+            Networkmanager.ClientName = NamePromptText.Text;
+
+            ContentControlActions.setPopup(new OnlineLobbyUserControl());
         }
     }
 }
